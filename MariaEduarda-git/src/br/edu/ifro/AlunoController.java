bfb1a2102cce81c0b930f5524f7ba2a4403144bc
86175033272 <86175033272@JIPA-LABIN05-13.ifro.local>
2018-05-14 08:52:29 -0400

    Mapeamento do banco de dados, criação da tela de cadaro e menu da tela principal, inserir no banco de apartir da tela de cadatro; Finalizados

---
+++ MariaEduarda-git/src/br/edu/ifro/AlunoController.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifro;
+
+import br.edu.ifro.Modelo.Aluno;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ * FXML Controller class
+ *
+ * @author 86175033272
+ */
+public class AlunoController implements Initializable {
+
+    @FXML
+    private TextField txtNome;
+ 
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    } 
+    
+    @FXML
+    private void Salvar (ActionEvent event){
+        EntityManagerFactory ent = Persistence.createEntityManagerFactory("aula");
+        EntityManager em = ent.createEntityManager();
+        
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome(txtNome.getText());
+              
+        em.getTransaction().begin();
+        
+        em.persist(aluno1);
+        
+        em.getTransaction().commit();
+        
+    }
+    
+    @FXML
+    private void Fechar (ActionEvent event){
+         Stage stage = (Stage) txtNome.getScene().getWindow();
+    stage.close();
+    
+    }
+}
